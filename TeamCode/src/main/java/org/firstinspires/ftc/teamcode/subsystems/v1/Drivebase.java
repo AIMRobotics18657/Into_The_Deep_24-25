c266db58fd36dc505a86fbce9bf22c397543fe39
WalnutCode <bbezoza@gmail.com>
2024-12-17 19:40:01 -0500

    creating griffy 2.0 folders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/v1/Drivebase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/v1/Drivebase.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.subsystems;
+package org.firstinspires.ftc.teamcode.subsystems.v1;
 
 import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.Vector2d;
