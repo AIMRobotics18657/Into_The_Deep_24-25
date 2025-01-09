a330ea54d3fa21876916e06ff827996ac2650dfe
julianghiazza <julian.ghiazza@gmail.com>
2025-01-08 19:40:56 -0500

    Blue Auto MeepMeep 3+1

--- MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTesting.java
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTesting.java
@@ -45,46 +45,70 @@ public class MeepMeepTesting {
 
 
         //RED SIDE 4 HANG
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(12, -62, Math.toRadians(90)))
-                // Specimen Hang 1
-                .strafeTo(new Vector2d(10, -33))
+//        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(12, -62, Math.toRadians(90)))
+//                // Specimen Hang 1
+//                .strafeTo(new Vector2d(10, -33))
+//
+//                // Left Block PUSH
+//                .splineToLinearHeading(new Pose2d(22, -38, Math.toRadians(90)), Math.toRadians(0))
+//                .splineToLinearHeading(new Pose2d(36, -26, Math.toRadians(90)), Math.toRadians(90))
+//                .splineToLinearHeading(new Pose2d(36, -6, Math.toRadians(90)), Math.toRadians(0))
+//                .splineToLinearHeading(new Pose2d(42, -48, Math.toRadians(90)), Math.toRadians(90))
+//
+////                 Middle Block PUSH
+//                .splineToLinearHeading(new Pose2d(50, -13, Math.toRadians(90)), Math.toRadians(0))
+//                .splineToLinearHeading(new Pose2d(54, -50, Math.toRadians(90)), Math.toRadians(90))
+//
+////                 Right Block PUSH
+//                .splineToLinearHeading(new Pose2d(57, -10, Math.toRadians(90)), Math.toRadians(0))
+//                .splineToLinearHeading(new Pose2d(61, -25, Math.toRadians(90)), Math.toRadians(100000000))
+//                .splineToLinearHeading(new Pose2d(61, -55, Math.toRadians(90)), Math.toRadians(100000000))
+//
+//
+////                 HANG 2
+//                .splineToLinearHeading(new Pose2d(42, -63, Math.toRadians(90)), Math.toRadians(100000000))
+//                .waitSeconds(1)
+//                .strafeTo(new Vector2d(8, -33))
+//                .waitSeconds(1)
+//
+////                 HANG 3
+//                .strafeTo(new Vector2d(42, -63))
+//                .waitSeconds(1)
+//                .strafeTo(new Vector2d(4, -33))
+//                .waitSeconds(1)
+//
+////                 HANG 4
+//                .strafeTo(new Vector2d(42, -63))
+//                .waitSeconds(1)
+//                .strafeTo(new Vector2d(0, -33))
+//
+//                .build());
 
-                // Left Block PUSH
-                .splineToLinearHeading(new Pose2d(22, -38, Math.toRadians(90)), Math.toRadians(0))
-                .splineToLinearHeading(new Pose2d(36, -26, Math.toRadians(90)), Math.toRadians(90))
-                .splineToLinearHeading(new Pose2d(36, -6, Math.toRadians(90)), Math.toRadians(0))
-                .splineToLinearHeading(new Pose2d(42, -48, Math.toRadians(90)), Math.toRadians(90))
+//BLUE SIDE 3 + 1
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(12, 62, Math.toRadians(270)))
+                // Specimen Hang
+                .strafeTo(new Vector2d(10, 33))
+                .waitSeconds(2)
 
-//                 Middle Block PUSH
-                .splineToLinearHeading(new Pose2d(50, -13, Math.toRadians(90)), Math.toRadians(0))
-                .splineToLinearHeading(new Pose2d(54, -50, Math.toRadians(90)), Math.toRadians(90))
+                // Right Block Pickup AND Drop
+                .splineToLinearHeading(new Pose2d(10, 42, Math.toRadians(90)), Math.toRadians(180))
+                .splineToLinearHeading(new Pose2d(48, 48, Math.toRadians(90)), Math.toRadians(70))
+                .waitSeconds(2)
+                .splineToLinearHeading(new Pose2d(56, 56, Math.toRadians(45)), Math.toRadians(90))
+                .waitSeconds(3.5)
 
-//                 Right Block PUSH
-                .splineToLinearHeading(new Pose2d(57, -10, Math.toRadians(90)), Math.toRadians(0))
-                .splineToLinearHeading(new Pose2d(61, -25, Math.toRadians(90)), Math.toRadians(100000000))
-                .splineToLinearHeading(new Pose2d(61, -55, Math.toRadians(90)), Math.toRadians(100000000))
+                // Middle Block Pickup AND Drop
+                .splineToLinearHeading(new Pose2d(58, 48, Math.toRadians(90)), Math.toRadians(90))
+                .waitSeconds(2)
+                .splineToLinearHeading(new Pose2d(56, 56, Math.toRadians(45)), Math.toRadians(90))
+                .waitSeconds(3.5)
 
-
-//                 HANG 2
-                .splineToLinearHeading(new Pose2d(42, -63, Math.toRadians(90)), Math.toRadians(100000000))
-                .waitSeconds(1)
-                .strafeTo(new Vector2d(8, -33))
-                .waitSeconds(1)
-
-//                 HANG 3
-                .strafeTo(new Vector2d(42, -63))
-                .waitSeconds(1)
-                .strafeTo(new Vector2d(4, -33))
-                .waitSeconds(1)
-
-//                 HANG 4
-                .strafeTo(new Vector2d(42, -63))
-                .waitSeconds(1)
-                .strafeTo(new Vector2d(0, -33))
+                // Left Block Pickup AND Drop
+                .splineToLinearHeading(new Pose2d(56, 44, Math.toRadians(125)), Math.toRadians(90))
+                .waitSeconds(2)
+                .splineToLinearHeading(new Pose2d(56, 56, Math.toRadians(45)), Math.toRadians(90))
 
                 .build());
-
-
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
